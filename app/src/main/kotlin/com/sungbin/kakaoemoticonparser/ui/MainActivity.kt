96b8f730f3cdf0ee6ce8e54c32d6301d35fb192b
jisungbin <ji@sungb.in>
2021-03-16 13:58:44 +0900

    ~~

--- app/src/main/kotlin/com/sungbin/kakaoemoticonparser/ui/MainActivity.kt
+++ app/src/main/kotlin/com/sungbin/kakaoemoticonparser/ui/MainActivity.kt
@@ -5,37 +5,98 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.Crossfade
 import androidx.compose.foundation.layout.Column
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Favorite
+import androidx.compose.material.icons.outlined.Search
+import androidx.compose.material.icons.outlined.Settings
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import com.sungbin.kakaoemoticonparser.R
+import com.sungbin.kakaoemoticonparser.ui.composable.RotateIcon
+import com.sungbin.kakaoemoticonparser.ui.home.HomeContent
+import com.sungbin.kakaoemoticonparser.ui.test.TestContent
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContent {
-        }
+        setContent { MainContent() }
     }
 
     @Composable
     private fun MainContent() {
-        val navigationState = rememberSaveable { mutableStateOf(NavigationType.HOME) }
+        val navigationState = rememberSaveable { mutableStateOf(NavigationType.SEARCH) }
 
         Column {
         }
     }
 
     @Composable
-    private fun NavigationContent(contentType: NavigationType) {
-        Column {
+    private fun NavigationFragmentContent(
+        modifier: Modifier = Modifier,
+        contentType: NavigationType
+    ) {
+        Column(modifier = modifier) {
             Crossfade(contentType) { type ->
                 when (type) {
-                    NavigationType.HOME -> HomeScreen()
+                    NavigationType.SEARCH -> HomeContent()
+                    else -> TestContent()
                 }
             }
         }
     }
 
-
+    @Composable
+    private fun NavigationBarContent(
+        modifier: Modifier = Modifier,
+        contentType: MutableState<NavigationType>
+    ) {
+        val animate = remember { mutableStateOf(false) }
+        BottomNavigation(modifier = modifier) {
+            BottomNavigationItem(
+                icon = { Icon(imageVector = Icons.Outlined.Search, contentDescription = null) },
+                selected = contentType.value == NavigationType.SEARCH,
+                onClick = {
+                    contentType.value = NavigationType.SEARCH
+                    animate.value = false
+                },
+                label = { Text(text = stringResource(id = R.string.navigation_search)) }
+            )
+            BottomNavigationItem(
+                icon = {
+                    RotateIcon(
+                        state = animate.value,
+                        imageVector = Icons.Outlined.Favorite,
+                        angle = 720f,
+                        duration = 2000
+                    )
+                },
+                selected = contentType.value == NavigationType.FAVORITE,
+                onClick = {
+                    contentType.value = NavigationType.FAVORITE
+                    animate.value = true
+                },
+                label = { Text(text = stringResource(id = R.string.navigation_favorite)) }
+            )
+            BottomNavigationItem(
+                icon = { Icon(imageVector = Icons.Outlined.Settings, contentDescription = null) },
+                selected = contentType.value == NavigationType.SETTING,
+                onClick = {
+                    contentType.value = NavigationType.SETTING
+                    animate.value = false
+                },
+                label = { Text(text = stringResource(id = R.string.navigation_setting)) }
+            )
+        }
+    }
 }
--- app/src/main/kotlin/com/sungbin/kakaoemoticonparser/ui/home/HomeContent.kt
+++ app/src/main/kotlin/com/sungbin/kakaoemoticonparser/ui/home/HomeContent.kt
@@ -1,25 +1,34 @@
 package com.sungbin.kakaoemoticonparser.ui.home
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.airbnb.lottie.compose.LottieAnimation
 import com.airbnb.lottie.compose.LottieAnimationSpec
 import com.airbnb.lottie.compose.rememberLottieAnimationState
 import com.sungbin.kakaoemoticonparser.R
 
+@Preview
 @Composable
 fun HomeContent() {
     Scaffold(
@@ -45,14 +54,10 @@ fun HomeContent() {
 
 @Composable
 private fun BindHomeContent() {
+    var searchText = remember { mutableStateOf(TextFieldValue()) }
     val animationSpec = remember { LottieAnimationSpec.RawRes(R.raw.search) }
     val animationState =
         rememberLottieAnimationState(autoPlay = true, repeatCount = Integer.MAX_VALUE)
-    val lottieView = LottieAnimation(
-        composition = animationSpec,
-        state = animationState,
-        modifier = Modifier.height(50.dp).width(250.dp)
-    )
 
     Column(
         verticalArrangement = Arrangement.Center,
@@ -66,7 +71,19 @@ private fun BindHomeContent() {
             shape = RoundedCornerShape(dimensionResource(id = R.dimen.margin_half)),
             elevation = dimensionResource(id = R.dimen.margin_default)
         ) {
-
+            TextField(
+                value = searchText.value,
+                onValueChange = { searchText.value = it },
+                placeholder = { stringResource(id = R.string.main_search_emoticon) }
+            )
         }
+        LottieAnimation(
+            spec = animationSpec,
+            animationState = animationState,
+            modifier = Modifier
+                .height(50.dp)
+                .width(250.dp)
+                .padding(top = dimensionResource(id = R.dimen.margin_default))
+        )
     }
 }
