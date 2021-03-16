4796877b71876f93604ca5c87bca3efc3867ed5c
jisungbin <ji@sungb.in>
2021-03-16 20:06:27 +0900

    컴포즈 감 잡았다

--- app/src/main/kotlin/com/sungbin/kakaoemoticonparser/theme/Typography.kt
+++ app/src/main/kotlin/com/sungbin/kakaoemoticonparser/theme/Typography.kt
@@ -3,11 +3,14 @@ package com.sungbin.kakaoemoticonparser.theme
 import androidx.compose.material.Typography
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.sungbin.kakaoemoticonparser.R
 
 val typography = Typography(
+    defaultFontFamily = FontFamily(Font(R.font.nanumgothic)),
     body1 = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
--- app/src/main/kotlin/com/sungbin/kakaoemoticonparser/ui/home/HomeContent.kt
+++ app/src/main/kotlin/com/sungbin/kakaoemoticonparser/ui/home/HomeContent.kt
@@ -1,23 +1,24 @@
 package com.sungbin.kakaoemoticonparser.ui.home
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
-import androidx.compose.material.TextField
 import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -37,7 +38,7 @@ fun HomeContent() {
         topBar = {
             TopAppBar(
                 title = {
-                    Column() {
+                    Column {
                         Text(text = stringResource(R.string.app_name))
                         Text(
                             text = stringResource(R.string.copyright),
@@ -56,7 +57,7 @@ fun HomeContent() {
 
 @Composable
 private fun BindHomeContent() {
-    val searchText = remember { mutableStateOf(TextFieldValue()) }
+    var searchText by remember { mutableStateOf(TextFieldValue()) }
     val animationSpec = remember { LottieAnimationSpec.RawRes(R.raw.search) }
     val animationState =
         rememberLottieAnimationState(autoPlay = true, repeatCount = Integer.MAX_VALUE)
@@ -67,31 +68,40 @@ private fun BindHomeContent() {
         modifier = Modifier
             .padding(dimensionResource(R.dimen.margin_default))
     ) {
-        TextField(
+        OutlinedTextField(
             modifier = Modifier
-                .fillMaxWidth()
-                .background(
-                    Color.White,
-                    RoundedCornerShape(dimensionResource(R.dimen.margin_default))
-                ),
-            value = searchText.value,
-            onValueChange = { searchText.value = it },
+                .fillMaxWidth(),
+            value = searchText,
+            label = { Text(text = stringResource(R.string.main_search_emoticon)) },
+            onValueChange = { searchText = it },
             placeholder = { Text(stringResource(R.string.main_search_emoticon)) },
             trailingIcon = {
                 Icon(
                     imageVector = Icons.Default.Settings,
                     contentDescription = null,
-                    tint = Color.Gray
+                    tint = Color.Gray,
                 )
             }
         )
-        LottieAnimation(
-            spec = animationSpec,
-            animationState = animationState,
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
             modifier = Modifier
-                .height(50.dp)
-                .width(250.dp)
-                .padding(top = dimensionResource(R.dimen.margin_default))
-        )
+                .fillMaxHeight()
+        ) {
+            LottieAnimation(
+                spec = animationSpec,
+                animationState = animationState,
+                modifier = Modifier
+                    .height(50.dp)
+                    .width(250.dp)
+                    .padding(top = dimensionResource(R.dimen.margin_default))
+            )
+            Text(
+                text = stringResource(R.string.main_search_first),
+                style = typography.body1,
+                modifier = Modifier.padding(top = dimensionResource(R.dimen.margin_twice))
+            )
+        }
     }
 }
