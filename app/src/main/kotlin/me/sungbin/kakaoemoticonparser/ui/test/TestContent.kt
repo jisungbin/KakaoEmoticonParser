3067cc5560065a4d6c09dc2e6e35a6af85cd8d85
jisungbin <ji@sungb.in>
2021-03-19 03:13:03 +0900

    블로그 작업 해야지

--- app/src/main/kotlin/me/sungbin/kakaoemoticonparser/ui/test/TestContent.kt
+++ app/src/main/kotlin/me/sungbin/kakaoemoticonparser/ui/test/TestContent.kt
@@ -1,11 +1,12 @@
 package me.sungbin.kakaoemoticonparser.ui.test
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.res.stringResource
-import me.sungbin.kakaoemoticonparser.R
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun TestContent() {
-    Text(text = stringResource(id = R.string.app_name))
+    Text(text = "TODO", modifier = Modifier.padding(30.dp))
 }
